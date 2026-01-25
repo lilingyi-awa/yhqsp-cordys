b4eebd37959d2b7052fa528c443f347525bbaab1
li_lingyi <centreis@xiaoice.vsinger.com._.local>
2026-01-25 18:20:27 +0800

    IAS Account Support

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ class Registration(ORMBase):
     __tablename__ = "registration"
     userName: str = sa.Column(sa.String(45).with_variant(sa.String(45, 'ascii_bin'), 'mysql', 'mariadb'), primary_key=True)
     yunhuId: typing.Optional[int] = sa.Column(sa.BigInteger(), nullable=True, unique=True)
+    robotOwner: typing.Optional[int] = sa.Column(sa.BigInteger(), nullable=True)
     userId: str = sa.Column(sa.String(45).with_variant(sa.String(45, 'ascii_bin'), 'mysql', 'mariadb'), nullable=True, unique=True)
 
 class LoginRequests(ORMBase):
@@ -116,6 +117,7 @@ async def registration(uid: int, username: str, nickname: str):
             session.add(Registration(
                 userName=username,
                 yunhuId=uid,
+                robotOwner=uid,
                 userId=userId,
             ))
             await session.commit()
@@ -178,17 +180,25 @@ async def whoisthey(uid: int, query: str):
         )
     async with Session() as session:
         target = await session.scalar(sa.select(Registration).where(Registration.userName == query))
-        if target is not None:
-            target = target.yunhuId
-    if target is not None:
+    if target is None:
         await eapis.deliverMessage(
             uid=uid,
-            message=f"对方云湖UID：{target}",
+            message="未查询到记录！",
+        )
+    if target.yunhuId is not None:
+        await eapis.deliverMessage(
+            uid=uid,
+            message=f"账户名：{query}\n类型：用户账户\n云湖UID：{target.yunhuId}",
+        )
+    if target.robotOwner is not None:
+        await eapis.deliverMessage(
+            uid=uid,
+            message=f"账户名：{query}\n类型：IAS账户\n云湖UID（创建者）：{target.robotOwner}",
         )
     else:
         await eapis.deliverMessage(
             uid=uid,
-            message="未查询到记录！",
+            message="账户名：{query}\n类型：IAS账户\n云湖UID（创建者）：无",
         )
 
 @http.post("/yunhubot/receive")
@@ -214,6 +224,8 @@ async def accept(req: fastapi.Request, secret: str):
             ))
         if code["event"]["message"]["commandId"] == 2240:
             asyncio.create_task(quicklogin(int(code["event"]["sender"]["senderId"])))
+        if code["event"]["message"]["commandId"] == 2273:
+            asyncio.create_task(quicklogin_ias(int(code["event"]["sender"]["senderId"]), code["event"]["message"]["content"]["text"]))
     if code["header"]["eventType"] == "bot.shortcut.menu":
         if code["event"]["menuId"] == "VO9SDAQ9":
             asyncio.create_task(quicklogin(int(code["event"]["senderId"])))
@@ -239,6 +251,41 @@ async def quicklogin(uid: int):
             await eapis.deliverMessage(
                 uid=uid,
                 message="您并未创建账户！",
+            )
+            return
+        rid = (int(time.time() * 1000 - 1767787667721) << 10) + random.randrange(0, 2048)
+        secret = ''.join(random.choice('1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLMNBVCXZ') for _ in range(0, 32))
+        session.add(LoginRequests(
+            rid=rid,
+            secret=secret,
+            userName=prereg.userName,
+        ))
+        del prereg
+        await session.commit()
+    await eapis.deliverMessage(
+        uid=uid,
+        message="请点击下面的按钮快捷登录（10分钟内有效，仅可使用一次）：",
+        buttons=[
+            {
+                "text": "登录",
+                "actionType": 1,
+                "url": f"https://{MISSKEY_DOMAIN}/yunhubot/vslogin/{rid}/{secret}"
+            }
+        ],
+    )
+
+async def quicklogin_ias(uid: int, name: str):
+    async with Session() as session:
+        if (prereg := await session.scalar(sa.select(Registration).where(Registration.userName == name))) is None:
+            await eapis.deliverMessage(
+                uid=uid,
+                message="IAS账户不存在！",
+            )
+            return
+        if prereg.userId != uid and prereg.robotOwner != uid:
+            await eapis.deliverMessage(
+                uid=uid,
+                message="不是您的IAS账户！",
             )
             return
         rid = (int(time.time() * 1000 - 1767787667721) << 10) + random.randrange(0, 2048)
